92558f6c0b5a79c754feef64a568ef2305289418
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-26 20:50:41 +0000

    new variable learn

---
+++ west/variables.js
@@ -0,0 +1,19 @@
+const accountId = 234567
+let accountEmail = "jsadfnfafdfnj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"// VARIABLE can be declare by this mathod but it is not preferable
+let accountState;
+
+//accountId = 2 not allowed
+
+accountEmail = "fdgmdfdf@com"
+accountPassword = "54321"
+accountCity = "Delhi"
+
+/*var is used to change the values throughtout the program
+let is used in place of var because the var has a problem of changing the scope of the program*/
+//ALWAYS PREFER LET
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
