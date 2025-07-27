f42a6cfdb1f92ed018d244545e6b2f7931c78fe4
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-27 09:36:13 +0000

    memory 2

--- west/memory.js
+++ west/memory.js
@@ -1,2 +1,4 @@
 //Stack memory is used by primitive
 //Heap memry is used by non primitive
+/*heap gives a reference to the original value and it is worked upon the original value
+Stack give a copy of the value to be worked upon*/
