4d7d76c85e0806caa219bd9375760bef67274b4a
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-27 11:25:01 +0000

    math and num in js

---
+++ west/mathNum.js
@@ -0,0 +1,33 @@
+ /*const score =400
+ console.log(score)
+
+ const balance =new Number(100)
+ console.log(balance)
+
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(2));
+
+ const otherNumber = 23.8966
+ console.log(otherNumber.toPrecision(3))
+
+ const hundreds = 1000000//in default form it takes english no. system
+ console.log(hundreds.toLocaleString('en-IN'));*/
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.min(4,5,6,78,32,0));
+console.log(Math.max(4,5,6,78,32,0));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const max=150
+const min=140
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
