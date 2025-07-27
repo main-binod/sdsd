d086c2999bf35af64520f5ae23827dc812cdb5b3
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-27 07:49:03 +0000

    dta types 2

--- west/dataTypes.js
+++ west/dataTypes.js
@@ -1,2 +1,18 @@
 "use strict";//treat all JS code as newer version
 //alert(3+3) // we are using nodejs,not browser
+let name="Anish Kuamr"
+let age=18
+let isLoggedIn = false
+let state = null
+
+//number=>2 to power 53
+//big int
+// string =>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object 
+
+console.log(typeof state);
