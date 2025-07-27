78f5916c11b13d1b909cfe227ee4751ad774107e
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-27 08:35:56 +0000

    data conversion in confusion

---
+++ west/dt_conversion.js
@@ -0,0 +1,55 @@
+let score = 33 //null->0 //undefined->NaN //true->1//false->0//string->NaN
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1//false=>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true
+//0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber=33
+
+let strinNumber=String(someNumber)
+//console.log(strinNumber)
+//console.log(typeof(strinNumber))
+
+
+/*******operations************* */
+
+let value=3
+let negValue = -value
+//console.log(negValue)
+
+ let s1="hello"
+ let s2=" hi"
+ let s3=s1+s2
+ //console.log(s3);
+
+ /*console.log("1"+2);//12
+ console.log(1+"2")//12
+ console.log("1"+2+2);//122
+ console.log(1+2+"2");//=32
+
+ console.log((3+4)*5%3);//=2 this is not prefeered at all times*/
+ 
+ let num1,num2,num3
+ num1 =num2=num3=2 + 2
+
+let gameChanger=99
+console.log(++gameChanger)
+console.log(gameChanger++)
+
+//read embm or sm
