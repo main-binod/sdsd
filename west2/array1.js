36b14dbac6eaa37b4d28737a6f50ec0528e69b10
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-27 11:58:06 +0000

    array1

---
+++ west2/array1.js
@@ -0,0 +1,7 @@
+// arrays in js are resaizeable
+// and cannot be fixed to any data type i.e. open to all
+
+const arr=[0,1,2,3,4,5,6,7,8,9]
+console.log(arr[0]);
+
+const my Heros = ["saktiman","naagraj"]
