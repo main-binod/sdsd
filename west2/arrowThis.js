fc196d694ec85ffae43ed819ef1e9aec717b18d0
main-binod <157886725+main-binod@users.noreply.github.com>
2025-07-28 16:09:27 +0000

    aroow function complete

--- west2/arrowThis.js
+++ west2/arrowThis.js
@@ -6,5 +6,51 @@ const user = {
 
     welcomeMessage:function(){
         console.log(`${this.username},welcome to website`);
+        console.log(this);
     }
 }
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// use of this in global ScriptProcessorNode
+// function chai(){
+//     let username="hitesh"
+//     console.log(this);
+// }
+// chai()
+
+// this is an arrow function
+const chai = () => {
+    let username="hitesh"
+    console.log(this);
+}
+// chai()
+
+// const <variable name>=() => {}
+// const addTwo = (num1+ num2)=>{
+//     return num1+num2
+// }
+// // const addTwo = {num1,num2} => (num1+num2) -----3
+
+// when the number in arrow function is taken inside the parenthesis then a return function is taken or
+// and when it is outside the parenthesis is used as 3
+// prefer parenthesis
+
+
+// immidiately invoked function expressions [IIFE]
+(function chai()
+{
+    console .log('DB CONNECTED');
+})();
+
+// ()()--first parenthesis is for definition and second one is used to wrap up the function
+
+( ()=>{
+    console.log
+(`DB BAKCHODI ${"name"}`);
+}
+('jdsfsnifnsf');
+// this is name iffi
